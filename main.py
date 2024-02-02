934bac40ab652da57be741633035f4691a6f27bd
sir-hobbyjog <jakeleetherealtor@gmail.com>
2024-02-01 21:34:36 -0700

    initial commit of jupyter notebook & set up App Token

--- main.py
+++ main.py
@@ -8,17 +8,9 @@ from sodapy import Socrata
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
-client = Socrata("data.cityofnewyork.us", None)
-
-# Example authenticated client (needed for non-public datasets):
-# client = Socrata(data.cityofnewyork.us,
-#                  MyAppToken,
-#                  username="user@example.com",
-#                  password="AFakePassword")
+client = Socrata("data.cityofnewyork.us", "yUnYRTjXHWdK6ATxbXOsJR52v")
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("qgea-i56i", limit=2000)
-
-# Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
+results = client.get("qgea-i56i", limit=5000)
+df = pd.DataFrame.from_records(results)
