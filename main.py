6fac5d5d47c77e1f76fdf8c0bd53f14321f94236
sir-hobbyjog <jakeleetherealtor@gmail.com>
2024-02-01 20:50:20 -0700

    Test commit - Jake initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from prophet import Prophet
+
+# API Connection
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata("data.cityofnewyork.us", None)
+
+# Example authenticated client (needed for non-public datasets):
+# client = Socrata(data.cityofnewyork.us,
+#                  MyAppToken,
+#                  username="user@example.com",
+#                  password="AFakePassword")
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("qgea-i56i", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
